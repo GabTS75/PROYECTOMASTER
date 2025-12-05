b88b6f72a19b002835b57aced2462d2d0e847fdb
GabTS75 <jgabriel.ts@gmail.com>
2025-12-05 19:22:44 +0100

    modificaciones

--- JAVASCRIPT/06.bucles.js
+++ JAVASCRIPT/06.bucles.js
@@ -1,12 +1,12 @@
 for (let i = 0; i < 3; i++) {
-    // console.log("Iteración número " + i);
+    // console.log("Iteración número " + i); // Imprime la iteración actual
     console.log('Hola', i); // Esto se repite 3 veces
 }
 console.log('Adios');
-// i = o -> True -> me llevo una -> Hola 0
+// i = 0 -> True -> me llevo una -> Hola 0
 // i = 1 -> True -> me llevo una -> Hola 1
 // i = 2 -> True -> me llevo una -> Hola 2
-// i = 3 -> False -> Salgo del bucle -> Adios
+// i = 3 -> False -> Salgo del bucle -> Adios (fin del bucle)
 
 const animales = [
     'Perro', // índice 0
@@ -20,6 +20,6 @@ const animales = [
 // Recorremos el array de animales
 for (let i = 0; i < animales.length; i++) {
     const animal = animales[i];
-    //console.log('Animal:', animales[i]); // Accedemos a cada animal por su índice
     console.log(animal, i); // Imprimimos el nombre del animal y su índice
+    // console.log('Animal:', i , animales[i]); // Accedemos a cada animal por su índice
 }
